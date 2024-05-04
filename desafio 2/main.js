19eefd103ca2e8691d83be9cdfe4a7e26f5e98e4
Josefina Daza <dazajosefina31@gmail.com>
2024-05-04 01:55:27 -0300

    desafio 3

---
+++ desafio 2/main.js
@@ -0,0 +1,5 @@
+let nombreProducto = "Pollera"
+let precioUnidad = 500
+let cantidadDeseada = prompt("Indique la cantidad deseada:")
+let precioTotal = precioUnidad * cantidadDeseada
+alert("Su total es $" + precioTotal +". Resumen: " + nombreProducto + " por la cantidad de " + cantidadDeseada )
