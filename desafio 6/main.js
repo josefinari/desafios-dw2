f5adcd27baf0ec7065700170da4da2ab23484ccb
Josefina Daza <dazajosefina31@gmail.com>
2024-05-24 18:39:22 -0300

    desafio 6

---
+++ desafio 6/main.js
@@ -0,0 +1,11 @@
+let producto = {
+    nombre : "Remera",
+    precio : "4500",
+    stock : "300"
+}
+
+producto.nombre = "Zapatillas"
+producto.categoria = "Calzado"
+delete producto.stock
+
+console.log (producto)
