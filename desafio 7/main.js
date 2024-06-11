4ee5587c966c067e39f0487b7f6de3c3d8e0af93
Josefina Daza <dazajosefina31@gmail.com>
2024-06-10 21:41:25 -0300

    desafio 7

---
+++ desafio 7/main.js
@@ -0,0 +1,5 @@
+let precio = document.getElementById(`precioProducto`)
+precio.innerText = `Precio: $450`
+let titulo = document.querySelector(`#nombreProducto`)
+titulo.style.color = "red";
+titulo.style.backgroundColor = "blue"
